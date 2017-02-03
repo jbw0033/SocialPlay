5f6e78a2da5a978d7d89f7da74de75bbbf42587e
Jeremy Albert <jeremy3681@gmail.com>
2017-02-03 14:14:58 -0800

    moved the bluetooth checks to the Client/Server Fragment

--- app/src/main/java/edu/uwb/css/a545/project/socialplay/ChooseGameFragment.java
+++ app/src/main/java/edu/uwb/css/a545/project/socialplay/ChooseGameFragment.java
@@ -18,31 +18,14 @@ import android.view.ViewGroup;
 
 public class ChooseGameFragment extends Fragment {
 
-    BluetoothAdapter mBluetoothAdapter;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.choose_server_page, container, false);
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            new AlertDialog.Builder(getActivity())
-                    .setTitle("Not compatible")
-                    .setMessage("Your phone does not support Bluetooth")
-                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            System.exit(0);
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }
 
-//        if (!mBluetoothAdapter.isEnabled()) {
-//            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
-//        }
 
         return view;
     }
--- app/src/main/java/edu/uwb/css/a545/project/socialplay/JoinCreateFragment.java
+++ app/src/main/java/edu/uwb/css/a545/project/socialplay/JoinCreateFragment.java
@@ -1,8 +1,12 @@
 package edu.uwb.css.a545.project.socialplay;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +17,7 @@ import android.widget.Button;
  */
 
 public class JoinCreateFragment extends Fragment {
-
+    BluetoothAdapter mBluetoothAdapter;
     Button mClientButton;
 //    Button mServerButton;
 
@@ -26,6 +30,24 @@ public class JoinCreateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.join_page, container, false);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, 1);
+        }
 
         mClientButton = (Button) view.findViewById(R.id.client_button);
 //        mServerButton = (Button) view.findViewById(R.id.server_button);
