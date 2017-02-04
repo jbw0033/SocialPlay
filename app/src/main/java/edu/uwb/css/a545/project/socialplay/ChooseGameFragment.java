87bc85b0c8ece4ea3f81fdbffb363cd03ce7c418
Jeremy Albert <jeremy3681@gmail.com>
2017-02-03 16:53:35 -0800

    The client page finds nearby bluetooth devices

--- app/src/main/java/edu/uwb/css/a545/project/socialplay/ChooseGameFragment.java
+++ app/src/main/java/edu/uwb/css/a545/project/socialplay/ChooseGameFragment.java
@@ -1,8 +1,12 @@
 package edu.uwb.css.a545.project.socialplay;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +14,12 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
+
+import static android.R.attr.filter;
 
 
 /**
@@ -18,15 +28,36 @@ import android.view.ViewGroup;
 
 public class ChooseGameFragment extends Fragment {
 
-
+    ArrayAdapter<String> listAdapter;
+    IntentFilter filter;
+    BroadcastReceiver receiver;
+    ListView list;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.choose_server_page, container, false);
 
+        listAdapter=new ArrayAdapter<>(this.getContext(),android.R.layout.simple_list_item_1,0);
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        list = (ListView) view.findViewById(R.id.deviceList);
+        list.setAdapter(listAdapter);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                    BluetoothDevice device = intent.getParcelableExtra( BluetoothDevice.EXTRA_DEVICE);
+                    listAdapter.add(device.getName()+"\n"+device.getAddress());
+                }
+            }
+        };
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getActivity().registerReceiver(receiver,filter);
 
-
+        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
+        btAdapter.startDiscovery();
         return view;
     }
 }
